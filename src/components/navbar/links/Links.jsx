e84b4bd367d23f818a8c13ba8a414139f23e895f
Eze-Samuel <ezeilosamuel143@gmail.com>
2024-04-22 12:18:48 +0100

    added the blog

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,59 +1,64 @@
-"use client"
+"use client";
+
 import { useState } from "react";
-import styles from "./links.module.css"
-import NavLink from "./navLink/navLink"
-import { RxHamburgerMenu } from "react-icons/rx";
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
+import { handleLogout } from "@/lib/action";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const links = [
-    {
-        title: "Homepage",
-        path: "/",
-    },
-    {
-        title: "About",
-        path: "/about",
-    },
-    {
-        title: "Contacts",
-        path: "/contact",
-    },
-    
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
 ];
 
-const Links = () => {
-    const [open, setOpen] = useState(false)
+const Links = ({session}) => {
+  const [open, setOpen] = useState(false);
 
-    const session = true
-    const isAdmin = true
+  // TEMPORARY
+  // const session = true;
+  // const isAdmin = true;
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.links}>
-                {links.map((link) => (
-                    <NavLink item={link} key={link.title} /> 
-                ))}{
-                     session ? (
-                        <>
-                          {isAdmin && <NavLink item={{title:"Admin", path:"/admin"}}/>}
-                       <button className={styles.logout}>Logout</button>
-                        </>
-                    ) : (
-                        <NavLink item={{title :"login", path:"/login"}} />
-                    )}
-            </div>
-            <button className={styles.menuButton} onClick={()=> setOpen((prev)=>!prev)}>
-                <RxHamburgerMenu />
-            </button>
-            {
-                open && 
-                <div className={styles.mobileLinks }>
-                    {links.map((link) => (
-                        <NavLink item={link} key={link.title} />
-                    ))}
-                </div>
-            }
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session?.user ? (
+          <>
+            {session.user?.isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <form action={handleLogout}>
+              <button className={styles.logout}>Logout</button>
+            </form>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+        <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}><GiHamburgerMenu/></button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Links;
